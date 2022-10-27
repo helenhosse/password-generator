1f03a0945f63fdb736a38d806c25a48db112d17d
helenhosse <helenhosse@gmail.com>
2022-10-26 19:39:18 -0600

    third commit

--- script.js
+++ script.js
@@ -3,6 +3,48 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+function generatePassword (){
+  let passwordLength = parseInt( window.prompt("provide length for password between 8-128 characters"));
+  console.log("passwordLength = ", passwordLength)
+  if (passwordLength > 128 || passwordLength < 8){
+    window.alert("Password length must be between 8 and 128 characters.");
+    return ("Password not generated.")
+  }
+
+  let possibleCharacters = "";
+  let lower = "abcdefghijklmnopqrstuvwxyz"; 
+  let includeLc = window.confirm("Press OK to include lowercase letters in password.")
+  console.log("includeLc = ", includeLc)
+  if (includeLc === true){
+    possibleCharacters += lower;
+  }
+  console.log("possibleCharacters = ", possibleCharacters)
+
+  let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+  let includeUc = window.confirm("Press OK to include uppercase letters in password.")
+  console.log("includeUc = ", includeUc)
+  if (includeUc === true){
+    possibleCharacters += upper;
+  }
+  console.log("possibleCharacters = ", possibleCharacters)
+
+  let numbers = "0123456789"; 
+  let includeNum = window.confirm("Press OK to include numerals in password.")
+  console.log("includeNum = ", includeNum)
+  if (includeNum === true){
+    possibleCharacters += numbers;
+  }
+  console.log("possibleCharacters = ", possibleCharacters)
+
+  let specialCharacters = "()*?.,!@_~/-+"; 
+  let includeSp = window.confirm("Press OK to include special characters in password.")
+  console.log("includeSp = ", includeSp)
+  if (includeSp === true){
+    possibleCharacters += specialCharacters;
+  }
+  console.log("possibleCharacters = ", possibleCharacters)
+
+}
 
 // Write password to the #password input
 function writePassword() {
