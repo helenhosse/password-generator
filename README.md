f0b7e79b37b13ae421abb61dd29ab18603f057b2
helenhosse <helenhosse@gmail.com>
2022-10-31 22:44:22 -0600

    added photos to READMD

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# password-generator
+# Semantic-Code-Challenge1
+
+## USER Story
+
+AS A marketing agency
+I WANT a codebase that follows accessibility standards
+SO THAT our own site is optimized for search engines
+
+## Description
+
+In this challenge, I wanted to correct the structure of the HTML and CSS file to being semantic. I have provided all changes made in the CSS and HTML files as comments on each section. Lastly, I condensed all of the rules in the CSS file to make it more managable. 
+
+## Installation
+
+I have provided 2 links at the end of this page that should be able to work on any device.
+
+## Screenshot of Changed Code
+
+I have attached the coding changes I have made from the orginial script. I did not include the original script in the screenshots.
+
+<img src="./assests/images/password-changes1.PNG" alt=shows the changes made to the top part of the JS sheet.>
+<img src="./assests/images/password-changes2.PNG" alt=shows the changes made to the middle section of JS sheet.>
+<img src="./assests/images/password-changes3.PNG" alt=shows the final part of the changes made to the JS sheet.>
+
+## The Result
+
+I have updated the existing code to allow for a more efficient and semantically structured website for the client. This should be accessible on all devices.
+
+## Links for Submission
+
+Deployed website link: https://helenhosse.github.io/password-generator/
+
+GitHub repository link: https://github.com/helenhosse/password-generator
+
+## Credit
+
+I referenced our class materials and received help from our TAs and my tutor Suresh Kumar.
--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Assignment code here
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,6 +8,8 @@ function generatePassword (){
     window.alert("Password length must be between 8 and 128 characters.");
     return ("Password not generated.")
   }
+  // In this section I made sure that the password length would be between 8 and 128 characters. If it was not
+  //between these numbers, I added the return section of code to display "Password not generated." on the screen
 
   let possibleCharacters = "";
   let lower = "abcdefghijklmnopqrstuvwxyz"; 
@@ -20,6 +20,9 @@ function generatePassword (){
   }
   console.log("possibleCharacters = ", possibleCharacters)
 
+  // In this section I added the option to add lowercase letters in the password generated. 
+  // The includeLC section of the code shows that the lowercase letters are only added if selected yes (or true)
+
   let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
   let includeUc = window.confirm("Press OK to include uppercase letters in password.")
   console.log("includeUc = ", includeUc)
@@ -27,6 +30,10 @@ function generatePassword (){
     possibleCharacters += upper;
   }
   console.log("possibleCharacters = ", possibleCharacters)
+
+  // In this section I added the option to add uppercase letters in the password generated. 
+  // The includeLC section of the code shows that the uppercase letters are only added if selected yes (or true)
+
 
   let numbers = "0123456789"; 
   let includeNum = window.confirm("Press OK to include numerals in password.")
@@ -36,6 +43,10 @@ function generatePassword (){
   }
   console.log("possibleCharacters = ", possibleCharacters)
 
+  // In this section I added the option to add numbers in the password generated. 
+  // The includeLC section of the code shows that numbers are only added if selected yes (or true)
+
+
   let specialCharacters = "()*?.,!@_~/-+"; 
   let includeSp = window.confirm("Press OK to include special characters in password.")
   console.log("includeSp = ", includeSp)
@@ -44,7 +55,31 @@ function generatePassword (){
   }
   console.log("possibleCharacters = ", possibleCharacters)
 
+ // In this section I added the option to add special characters in the password generated. 
+  // The includeLC section of the code shows that the special characters are only added if selected yes (or true)
+
+
+
+  if (includeLc === false && includeUc === false && includeNum === false && includeSp === false){
+    alert("At least one character type should be chosen.");
+    return ("Password not generated.")
+  }
+
+  let password = "";
+
+  //This shows that the password generation has to have one of the following selected as yes (true) in order for it to run
+  //If all are selected as no, it will show as "Password not generated."
+
+  for (let i = 0; i < passwordLength; i++ ){
+    let randomIndex = Math.floor (Math.random() *  possibleCharacters.length);
+    password += possibleCharacters[randomIndex];
+  }
+
+  return password;
 }
+
+// This section is added to generate the actual password if at least one of the sections as been selected as yes
+
 
 // Write password to the #password input
 function writePassword() {
@@ -55,5 +90,11 @@ function writePassword() {
 
 }
 
+// This section is added to generate the actual password if at least one of the sections as been selected as yes
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// The event listener is needed to start the process of generating the code when the button generate password is pressed
+// Without this nothing will pop up when the button is pressed
